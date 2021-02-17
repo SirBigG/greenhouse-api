661fcc5b33d596d54dfed4c66578bd6e05b753ef
SirBigG <gotsandriy@gmail.com>
2021-02-17 10:55:42 +0200

    added cors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
 
@@ -10,6 +11,23 @@ from aioinflux import InfluxDBClient
 
 
 app = FastAPI()
+
+origins = [
+    "http://greenhouse-api.agromega.in.ua"
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:53981",
+    "http://localhost:57489",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 influx_client = InfluxDBClient(host="influxdb", db=os.getenv('INFLUXDB_DB', ''))
 
@@ -44,9 +62,9 @@ async def save_temperature(item: Humidity):
 
 @app.get("/temperature")
 async def get_temperature():
-    return await influx_client.query('SELECT value FROM temperature WHERE time > now() - 2h')
+    return await influx_client.query('SELECT * FROM temperature WHERE time > now() - 2h')
 
 
 @app.get("/humidity")
 async def get_temperature():
-    return await influx_client.query('SELECT value FROM humidity WHERE time > now() - 2h')
+    return await influx_client.query('SELECT * FROM humidity WHERE time > now() - 2h')
